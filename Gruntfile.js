ea828e35e8efe8e8591140c21403bd54564266e4
Marty Kane <marty@pixotech.com>
2014-07-02 23:39:06 -0500

    Also watch index.html

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,6 +27,9 @@ module.exports = function(grunt) {
             options: {
                 spawn: false,
             },
+        },
+        html : {
+            files: ['*.html']
         } 
     }
 
